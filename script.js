0cdbe996e4ab0ddf100bd130d617ed71715c3ca8
Joy-Kimani <kizengajoy@gmail.com>
2024-09-03 20:26:58 +0300

    add features

---
+++ script.js
@@ -0,0 +1,9 @@
+/*add functionaites for the sign up page
+
+listen for events
+when a user inputs their password they are able to see it or hide at will
+where will i save the password that has been put in- create a database for that
+how do I make sure that if there is already an account with that username, then show them a message saying "username taken"
+how do you login using google
+how does the forget password help you to recover your account?what is the user required to how to quide them step wise.
+*/
